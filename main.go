6dd098152396f96bebd577eaeeebd228632a621f
hxxvyweight <linuxtest923@gmail.com>
2026-09-07 17:56:24 +1000

    feat: move seed data from go to seed.sql

--- main.go
+++ main.go
@@ -111,48 +111,6 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func seedDatabase(db *sql.DB) error {
-
-	var venueCount int
-	if err := db.QueryRow("SELECT COUNT(*) FROM venues").Scan(&venueCount); err == nil && venueCount == 0 {
-		venueSeed := `
-			INSERT INTO venues (name, location, region, link) VALUES 
-			('Heyday', '270 Crown St', 'wollongong', 'https://heyday.com.au'),
-			('UOW UniBar', 'Building 12, Northfields Ave', 'wollongong', 'https://unibar.uow.edu.au'),
-			('The Grand Hotel', '32 Spencer St', 'illawarra', ''),
-			('Society', 'Keira St', 'wollongong', ''),
-			('Rad Bar (Archive)', 'Formatting/Defunct Site', 'wollongong', ''),
-			('La La La''s', 'Globe Lane', 'wollongong', 'https://lalalas.com.au');`
-
-		if _, err := db.Exec(venueSeed); err != nil {
-			return err
-		}
-		log.Println("Seeded venues successfully.")
-	}
-
-	var artistCount int
-	if err := db.QueryRow("SELECT COUNT(*) FROM artists").Scan(&artistCount); err == nil && artistCount == 0 {
-
-		artistSeed := `
-	INSERT INTO artists (name, dj_name, label, location, region, genre, link) VALUES 
-	('Sylent', 'Sylent', 'Independent', 'Wollongong', 'Illawarra', 'house/ukg/techno/dnb', 'https://www.instagram.com/sylentau'),
-	('COVE Sound System', 'COVE', 'Independent', 'Wollongong', 'Illawarra', 'house/techno/ukg', 'https://instagram.com/covesoundsystem'),
-	('Carlo', 'Carlo', NULL, 'Wollongong', 'Illawarra', 'tech house', NULL),
-	('Dubs', 'Dubs', NULL, 'Wollongong', 'Illawarra', 'tech house/deep house', NULL),
-	('Thabo', 'Thabo', NULL, 'Wollongong', 'Illawarra', 'techno/deep house', NULL),
-	('Motorik Vibe Council', 'Motorik Vibe Council', 'Motorik!', 'Wollongong', 'Illawarra', 'techno/house', NULL),
-	('Staffy', 'Staffy', NULL, 'Wollongong', 'Illawarra', 'house/tech house', NULL),
-	('Tom Carroll', 'Tom Carroll', NULL, 'Wollongong', 'Illawarra', 'house/techno', NULL);`
-
-		if _, err := db.Exec(artistSeed); err != nil {
-			return err
-		}
-		log.Println("Seeded Illawarra artists successfully.")
-	}
-	return nil
-
-}
-
 func handleVenues(db *sql.DB) http.HandlerFunc {
 
 	return func(w http.ResponseWriter, r *http.Request) {
